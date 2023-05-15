0dcd585de04d4f86c992c6159982a5e72cba1109
S.Uslisty <touchar2@yandex.ru>
2023-05-15 23:26:39 +0300

    added web validation file

--- web_handler.py
+++ web_handler.py
@@ -1,26 +1,18 @@
 from requests import head as requests_head
-from rich.console import Console
-
-rich_console = Console().print
 
 
 def check_site(site):
-    """Проверка доступности Caffesta Beanstalk"""
 
-    rich_console(f'Проверка доступности {site}', style = 'bold red')
+    print(f'Проверка доступности {site}')
 
     try:
-        status = requests_head(site).status_code
-    except ConnectionError as UnexpecetedErr:
-        rich_console(f'Непредвиденная проблема подключения: {UnexpecetedErr}')
+        status = requests_head(site, allow_redirects=True).status_code
+    except ConnectionError as UnexpectedErr:
+        print(f'Непредвиденная проблема подключения: {UnexpectedErr}')
     else:
         match status:
             case 200:
                 return True
-            case 401:
-                return True
             case _:
-                rich_console(f"""[bold]Невозможно получить доступ к {site} из-за ошибки {status}[/bold]""",
-                             style = 'yellow')
-                return False
-
+                print(f"Невозможно получить доступ к {site} из-за ошибки {status}")
+                return check_site(site)
