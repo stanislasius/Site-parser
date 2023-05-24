084f860ab2646544d761b619f8c595ac741b95c0
S.Uslisty <touchar2@yandex.ru>
2023-05-24 23:46:10 +0300

    fix for exceptions processing

--- web_handler.py
+++ web_handler.py
@@ -1,7 +1,11 @@
+from requests import exceptions
 from requests import head as requests_head
+import verifications
+
 from rich.console import Console
 
 rich_console = Console().print
+
 
 def check_site(site):
 
@@ -9,12 +13,15 @@ def check_site(site):
 
     try:
         status = requests_head(site, allow_redirects=True).status_code
-    except ConnectionError as UnexpectedErr:
+    except exceptions.ConnectionError as UnexpectedErr:
         rich_console(f'Непредвиденная проблема подключения: {UnexpectedErr}', style='red')
+        rich_console('Повторите ввод URL сайта: ', style='yellow')
+        site = input()
+        return check_site(verifications.check_site_address(site))
     else:
         match status:
             case 200:
-                return True
+                return site
             case _:
                 rich_console(f"Невозможно получить доступ к {site} из-за ошибки {status}", style='red')
                 return check_site(site)
